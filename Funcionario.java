a71db3fd8d319bc216686a08849d60716c81625c
Otavio Augusto <114100449+Hoptavio@users.noreply.github.com>
2025-09-02 14:19:32 -0300

    Create Funcionario.java

---
+++ Funcionario.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Funcionario{
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite seu gênero: ");
+        char sexo = scanner.next().charAt(0);
+        sexo = Character.toLowerCase(sexo);
+        
+        System.out.print("Digite seu salário: ");
+        float salario = scanner.nextFloat();
+        int resultado;
+        if(sexo =='m'){
+            salario = salario * 0.05f; 
+            System.out.print("Aumento de salário no valor de: "+salario);
+        }
+        else{
+           salario = salario * 0.03f;
+           System.out.print("Aumento de salário no valor de: "+salario);
+        }
+        scanner.close();
+    }
+}

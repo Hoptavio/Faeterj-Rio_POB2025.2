88cd06b8754c66844b7ac0decdb82fcc768c3a81
Otavio Augusto <114100449+Hoptavio@users.noreply.github.com>
2025-09-03 11:11:49 -0300

    Create Ex2.java

---
+++ Lista1/Ex2.java
@@ -0,0 +1,15 @@
+package Lista1.Ex2;
+import java.util.Scanner;
+public class Ex2
+{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double pi = 3.14159;
+		System.out.print("Digite o raio: ");
+		int raio = scanner.nextInt();
+		System.out.print("==Area do circulo==");
+		double area = pi *(raio*raio);
+		System.out.print(area);
+	}
+	
+}

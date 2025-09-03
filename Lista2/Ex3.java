5fd68832cdaa888db1e089ebbb331c4c99d11504
Otavio Augusto <114100449+Hoptavio@users.noreply.github.com>
2025-09-03 14:44:55 -0300

    Create Ex3.java

---
+++ Lista2/Ex3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex3
+{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Digite o primeiro numero : ");
+		int num1 = scanner.nextInt();
+		
+		System.out.print("Digite o segundo numero : ");
+		int num2 = scanner.nextInt();
+		if(num1>=num2) {
+			System.out.print("o maior numero digitado foi: "+num1);
+		}
+		else {
+			System.out.print("o maior numero digitado foi: "+num2);
+		}
+		scanner.close();
+	}
+}
